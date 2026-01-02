ff77484ae137428047defb7212664f5091a2eef0
Fortune Nnah <Fortunennah747@gmail.com>
2026-01-02 03:18:37 +0100

    Revert "tried linking to supabase"
    This reverts commit c17f5d21dbcb5f6827555d59dcdd95819a577b33.

--- SupabaseClient.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = process.env.HTML_SUPABASE_URL
-const supabaseKey = process.env.HTML_SUPABASE_ANON_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
